78abb099ac8c652fa2e5556305af1d73055bc521
1570599338@qq.com <1570599338@qq.com>
2018-08-29 18:29:30 +0800

    提交

--- src/main/java/com/head/first/charp2/view/main/impl/WeatherData.java
+++ src/main/java/com/head/first/charp2/view/main/impl/WeatherData.java
@@ -45,15 +45,23 @@ public class WeatherData implements Subject{
      */
     @Override
     public void removeObserver(Observer o) {
-
+       int i = this.observers.indexOf(o);
+       if(i>=0){
+           this.observers.remove(i);
+       }
 
     }
 
     /**
      * 当主题状态改变时，这个方法会被调用，以通知所有的观察者。
-     */
+     *  有趣的地方：在这里，我们把状态告诉每一个观察者，因为观察者都实现了
+ update（），所以我们知道该如何通知    */
     @Override
     public void notifyObservers() {
+        for (int i = 0; i <observers.size() ; i++) {
+            Observer observer= (Observer)observers.get(i);
+        }
+
 
     }
 }
